931e8807f07cdfc77ea491f7f7e752ab989e04a3
Jackson Yin <Yrh7383111@outlook.com>
2020-10-16 17:24:45 -0400

    Aadd .gitignore

---
+++ lab2/cmake-build-debug/CMakeFiles/lab2.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/JacksonYin/Desktop/Waterloo/Academic/ECE 2B/ECE 252/Lab/ece252/lab2/code/helper.c" "/Users/JacksonYin/Desktop/Waterloo/Academic/ECE 2B/ECE 252/Lab/ece252/lab2/cmake-build-debug/CMakeFiles/lab2.dir/code/helper.c.o"
+  "/Users/JacksonYin/Desktop/Waterloo/Academic/ECE 2B/ECE 252/Lab/ece252/lab2/code/lab_png.c" "/Users/JacksonYin/Desktop/Waterloo/Academic/ECE 2B/ECE 252/Lab/ece252/lab2/cmake-build-debug/CMakeFiles/lab2.dir/code/lab_png.c.o"
+  "/Users/JacksonYin/Desktop/Waterloo/Academic/ECE 2B/ECE 252/Lab/ece252/lab2/code/paster.c" "/Users/JacksonYin/Desktop/Waterloo/Academic/ECE 2B/ECE 252/Lab/ece252/lab2/cmake-build-debug/CMakeFiles/lab2.dir/code/paster.c.o"
+  "/Users/JacksonYin/Desktop/Waterloo/Academic/ECE 2B/ECE 252/Lab/ece252/lab2/starter/cURL/main_simple.c" "/Users/JacksonYin/Desktop/Waterloo/Academic/ECE 2B/ECE 252/Lab/ece252/lab2/cmake-build-debug/CMakeFiles/lab2.dir/starter/cURL/main_simple.c.o"
+  "/Users/JacksonYin/Desktop/Waterloo/Academic/ECE 2B/ECE 252/Lab/ece252/lab2/starter/cURL/main_write_callback.c" "/Users/JacksonYin/Desktop/Waterloo/Academic/ECE 2B/ECE 252/Lab/ece252/lab2/cmake-build-debug/CMakeFiles/lab2.dir/starter/cURL/main_write_callback.c.o"
+  "/Users/JacksonYin/Desktop/Waterloo/Academic/ECE 2B/ECE 252/Lab/ece252/lab2/starter/cURL/main_write_header_cb.c" "/Users/JacksonYin/Desktop/Waterloo/Academic/ECE 2B/ECE 252/Lab/ece252/lab2/cmake-build-debug/CMakeFiles/lab2.dir/starter/cURL/main_write_header_cb.c.o"
+  "/Users/JacksonYin/Desktop/Waterloo/Academic/ECE 2B/ECE 252/Lab/ece252/lab2/starter/fn_ptr/main.c" "/Users/JacksonYin/Desktop/Waterloo/Academic/ECE 2B/ECE 252/Lab/ece252/lab2/cmake-build-debug/CMakeFiles/lab2.dir/starter/fn_ptr/main.c.o"
+  "/Users/JacksonYin/Desktop/Waterloo/Academic/ECE 2B/ECE 252/Lab/ece252/lab2/starter/getopt/main_getopt.c" "/Users/JacksonYin/Desktop/Waterloo/Academic/ECE 2B/ECE 252/Lab/ece252/lab2/cmake-build-debug/CMakeFiles/lab2.dir/starter/getopt/main_getopt.c.o"
+  "/Users/JacksonYin/Desktop/Waterloo/Academic/ECE 2B/ECE 252/Lab/ece252/lab2/starter/pthreads/main.c" "/Users/JacksonYin/Desktop/Waterloo/Academic/ECE 2B/ECE 252/Lab/ece252/lab2/cmake-build-debug/CMakeFiles/lab2.dir/starter/pthreads/main.c.o"
+  "/Users/JacksonYin/Desktop/Waterloo/Academic/ECE 2B/ECE 252/Lab/ece252/lab2/starter/times/main.c" "/Users/JacksonYin/Desktop/Waterloo/Academic/ECE 2B/ECE 252/Lab/ece252/lab2/cmake-build-debug/CMakeFiles/lab2.dir/starter/times/main.c.o"
+  "/Users/JacksonYin/Desktop/Waterloo/Academic/ECE 2B/ECE 252/Lab/ece252/lab2/starter/times/util_times.c" "/Users/JacksonYin/Desktop/Waterloo/Academic/ECE 2B/ECE 252/Lab/ece252/lab2/cmake-build-debug/CMakeFiles/lab2.dir/starter/times/util_times.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../starter/times"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
